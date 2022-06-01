f1691cae0c28f03e0dd629577eab793f5751c7f0
Kevin <kevin7796.kr@gmail.com>
2022-06-01 18:37:17 -0300

    Implementación del testing y del CI

---
+++ Codigo/test/rsc/JuegoTest.java
@@ -0,0 +1,21 @@
+package rsc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JuegoTest {
+
+//    @Test
+//    void multiplicar() {
+//        int result = Juego.Multiplicar(2);
+//        assertEquals(4,result);
+//        //assertEquals(5,result);
+//    }
+//
+//    @Test
+//    void sumar() {
+//        int result = Juego.Sumar(3);
+//        assertEquals(5,result);
+//    }
+}
