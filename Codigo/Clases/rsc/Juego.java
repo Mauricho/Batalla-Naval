84a23917a916d9ff69455d2aeede9b1e27956f6f
Kevin <kevin7796.kr@gmail.com>
2022-06-02 19:59:40 -0300

    Implementación del testing y del CI

--- Codigo/Clases/rsc/Juego.java
+++ Codigo/Clases/rsc/Juego.java
@@ -1,11 +1,11 @@
 package rsc;
 
 public class Juego {
-//    public static int Multiplicar(int x){
-//        return (2*x);
-//    }
-//    public static int Sumar(int x){
-//        return x+x;
-//
-//    }
+    public static int Multiplicar(int x){
+        return (2*x);
+    }
+    public static int Sumar(int x){
+        return x+x;
+
+    }
 }
--- Codigo/test/rsc/JuegoTest.java
+++ Codigo/test/rsc/JuegoTest.java
@@ -1,21 +1,20 @@
 package rsc;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class JuegoTest {
+public class JuegoTest {
 
-//    @Test
-//    void multiplicar() {
-//        int result = Juego.Multiplicar(2);
-//        assertEquals(4,result);
-//        //assertEquals(5,result);
-//    }
-//
-//    @Test
-//    void sumar() {
-//        int result = Juego.Sumar(3);
-//        assertEquals(5,result);
-//    }
+    @Test
+    public void multiplicar() {
+        int resultado = Juego.Multiplicar(2);
+        assertEquals(4,resultado);
+    }
+
+    @Test
+    public void sumar() {
+        int resultado = Juego.Sumar(3);
+        assertEquals(6,resultado);
+    }
 }
