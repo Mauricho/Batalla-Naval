277a1a220287fa3f674378147a1d4a167eb8311c
FacujesusM <facumartinez431@gmail.com>
2022-06-08 23:39:37 -0300

    08/06 23:39

---
+++ target/classes/rsc/BatallaNaval.java
@@ -0,0 +1,14 @@
+package rsc;
+
+public class BatallaNaval {
+    public static void main(String[] args){
+        try{
+            //Modelo modelo = modelo.getInstance();
+            //modelo.leerArchivo();
+            //modelo.iniciarVistaMenuPrincipal();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
