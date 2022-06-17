fa5e02dbecbccf81d18113c0b924f8f861e5ff01
FacujesusM <facumartinez431@gmail.com>
2022-06-17 00:45:38 -0300

    17/06 00:45

--- Codigo/Clases/src/Arma.java
+++ Codigo/Clases/src/Arma.java
@@ -5,17 +5,49 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
  * @author f_acu
  */
 public class Arma {
-    private int tipoArma;
-    private float probabilida;
+    private int tipoArma=0;
     
     public Arma(){
         
+    }
+    
+    public void setTipoArma(){
+        int res;
+        Random rand=new Random();
+        int upperBound=20;
+        int ran=rand.nextInt(upperBound);
+        if(ran==11 || ran == 13){
+            this.tipoArma=2;
+        }else if(ran%2!=0){
+            this.tipoArma=1;
+        }else if(ran==19){
+            this.tipoArma=3;
+        }else{
+            this.tipoArma=0;
+        }
+    }
+    
+    public ArrayList disparos(int x, int y){
+        ArrayList<Integer> res= new ArrayList();
+        switch(this.tipoArma){
+            case 1:
+                return disparoBomba(x, y);
+            case 2:
+                return disparoBomba(x, y);
+            case 3:
+                return disparoBomba(x, y);                
+            default: break;
+        }
+        int i=x*10+y;
+        res.add(i);
+        return res;
     }
     
     public ArrayList disparoBomba(int x, int y){
