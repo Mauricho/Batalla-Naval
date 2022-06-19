fb63fbf419aa8d8c60162eb3a14d6d7715a33330
FacujesusM <facumartinez431@gmail.com>
2022-06-19 14:38:49 -0300

    al fin

--- Codigo/Clases/src/controlador/CtrJuegoNormal.java
+++ Codigo/Clases/src/controlador/CtrJuegoNormal.java
@@ -4,8 +4,10 @@
  */
 package src.controlador;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
 import src.modelo.Modelo;
-
 /**
  *
  * @author f_acu
@@ -13,10 +15,52 @@ import src.modelo.Modelo;
 public class CtrJuegoNormal implements Controlador {
 
     private static Modelo modelo;
+    JPanel panelTab;
+    JPanel panelTxt;
+    JPanel panelJugador;
+    JButton[][] botones= new JButton[10][10];
+    JLabel turno;
 
-    public CtrJuegoNormal() {
+    public CtrJuegoNormal(JPanel panelTab, JPanel panelTxt, JPanel panelJugador, JLabel turno) {
         super();
+        this.panelTab = panelTab;
+        this.panelTxt = panelTxt;
+        this.panelJugador = panelJugador;
         this.modelo = Modelo.getInstance();
+        this.turno=turno;
+        crear_tablero();
+    }
+    
+    public void crear_tablero(){
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10 ; j++){
+                JButton btn_tablero=new JButton(".");
+                btn_tablero.setBounds(60*j, 60*i, 60, 60);
+                btn_tablero.setBackground(Color.gray);
+                btn_tablero.putClientProperty("x", i);
+                btn_tablero.putClientProperty("y", j);
+                btn_tablero.addActionListener(new java.awt.event.ActionListener() {                    
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (disparable((Integer)((JButton)e.getSource()).getClientProperty("x"), (Integer)((JButton)e.getSource()).getClientProperty("y"))){
+                            if(generarDisparoJugador((Integer)((JButton)e.getSource()).getClientProperty("x"), (Integer)((JButton)e.getSource()).getClientProperty("y"))){
+                                btn_tablero.setBackground(Color.red);
+                            }else{
+                                btn_tablero.setBackground(Color.blue);
+                            }
+                        } 
+                    }
+                });
+                
+                botones[i][j]=btn_tablero;
+            }
+        }
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10 ; j++){
+                panelTab.add(botones[i][j]);
+                panelTab.repaint();
+            }
+        }
     }
     
     public boolean generarDisparoJugador(int x, int y){
