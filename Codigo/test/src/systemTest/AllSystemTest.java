a260b8154792ee350c5c42f6d91f3a0b4fbdd259
KevinReynoso <kevin.reynoso@mi.unc.edu.ar>
2022-06-19 23:23:17 -0300

    Juego completo - Falta corregir Modo LuckShot

--- Codigo/test/src/systemTest/AllSystemTest.java
+++ Codigo/test/src/systemTest/AllSystemTest.java
@@ -9,4 +9,5 @@ import org.junit.runners.Suite.SuiteClasses;
         CaseButtonSalirTest.class, CaseButtonDestructorTest.class, CaseButtonGirarTest.class, CaseButtonIniciarPartidaTest.class, CaseButtonRendirseTest.class,
         CaseButtonJugarModoLuckShotTest.class, CaseButtonJugarModoClasicoTest.class})
 public class AllSystemTest {
+
 }
